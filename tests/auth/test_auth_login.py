155ff7c852fe2eac6f980aed3f9d3e5376262613
Seyeon Lim <seyeonlim8@gmail.com>
2025-09-30 14:36:57 -0400

    added invalid credentials test

--- tests/auth/test_auth_login.py
+++ tests/auth/test_auth_login.py
@@ -9,7 +9,36 @@ def test_login(driver, base_url, admin_email, admin_password):
     driver.find_element(By.XPATH, '//button[text()="Log In"]').click()
 
     WebDriverWait(driver, 5).until(EC.url_to_be(f"{base_url}/"))
-
     assert driver.current_url == f"{base_url}/"
 
-    
+    nav_hello = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='nav-hello']")))
+    assert nav_hello.text.startswith("Hello")
+
+def test_invalid_credentials(driver, base_url, test1_email, test1_password):
+    driver.get(f"{base_url}/login")
+    email_field = driver.find_element(By.XPATH, "//input[@placeholder='Email']")
+    password_field = driver.find_element(By.XPATH, "//input[@placeholder='Password']")
+    login_btn = driver.find_element(By.XPATH, '//button[text()="Log In"]')
+
+    # Invalid email, valid password
+    email_field.send_keys("randomemail@random.com")
+    password_field.send_keys(test1_password)
+    login_btn.click()
+    error_msg = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='credentials-error']"))
+    )
+    assert "Invalid email or password" in error_msg.text
+
+    email_field.clear()
+    password_field.clear()
+    email_field = driver.find_element(By.XPATH, "//input[@placeholder='Email']")
+    password_field = driver.find_element(By.XPATH, "//input[@placeholder='Password']")
+
+    # Valid email, invalid password
+    email_field.send_keys(test1_email)
+    password_field.send_keys("IncorrectPassword!123")
+    login_btn.click()
+    error_msg = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='credentials-error']"))
+    )
+    assert "Invalid email or password" in error_msg.text

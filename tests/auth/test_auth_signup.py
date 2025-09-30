e2192042e6f7a0108b5b4f793a2bfdfdd8c2df5c
Seyeon Lim <seyeonlim8@gmail.com>
2025-09-30 14:37:24 -0400

    added test_button_disabled test

--- tests/auth/test_auth_signup.py
+++ tests/auth/test_auth_signup.py
@@ -18,3 +18,25 @@ def test_signup(driver, base_url, test1_email, test1_password):
 
     WebDriverWait(driver, 5).until(EC.url_to_be(f"{base_url}/login"))
     assert driver.current_url == f"{base_url}/login"
+
+def test_button_disabled(driver, base_url, test1_email, test1_password):
+    driver.get(f"{base_url}/signup")
+    signup_btn = driver.find_element(By.XPATH, '//button[text()="Sign Up"]')
+
+    # All fields empty - btn disabled
+    assert signup_btn.is_enabled() is False
+
+    # Username only - btn disabled
+    signup_btn = driver.find_element(By.XPATH, '//button[text()="Sign Up"]')
+    driver.find_element(By.XPATH, "//input[@placeholder='Username']").send_keys("Test1")
+    assert signup_btn.is_enabled() is False
+
+    # Username, email only - btn disabled
+    signup_btn = driver.find_element(By.XPATH, '//button[text()="Sign Up"]')    
+    driver.find_element(By.XPATH, "//input[@placeholder='Email']").send_keys(test1_email)
+    assert signup_btn.is_enabled() is False
+
+    # All fields filled - btn enabled
+    signup_btn = driver.find_element(By.XPATH, '//button[text()="Sign Up"]') 
+    driver.find_element(By.XPATH, "//input[@placeholder='Password']").send_keys(test1_password)
+    assert signup_btn.is_enabled() is True
